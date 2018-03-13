c4f25fa96216ef270e44e62cede4cabc86e2173c
Elana Kopelevich <ekopelevich@gmail.com>
2018-03-12 20:58:49 -0600

    Wire up service
    - Move HTTP call to Show
    - show Github project names in Show

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -3,7 +3,7 @@ require 'rest-client'
 class GitHubService
 
   def self.search(search_term)
-    response = RestClient.get "https://api.github.com/search/repositories?q=topic:#{search_term}"
+    RestClient.get "https://api.github.com/search/repositories?q=topic:#{search_term.term}"
   end
   
 end
